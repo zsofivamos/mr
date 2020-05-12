3b085c7fc702ce8a205ffc0afe1ba0fc037d71a2
zsofivamos <zsofivamos@gmail.com>
2020-05-12 16:55:55 +0000

    added binance funtion and deleted hello file

--- R/binance.R
+++ R/binance.R
@@ -13,11 +13,4 @@ get_bitcoin_price <- function(retried = 0){
     })
 }
 
-#' Formatter function for Hungarian Forint
-#' @param x number
-#' @export
-#' @importFrom scales dollar
-forint <- function(x){
-  dollar(x, prefix = "", suffix = " Ft")
-}
 
